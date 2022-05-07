34b808ff5f4c8153b00326a1472bbeaae891903c
Werka <69664@student.ue.poznan.pl>
2022-05-07 20:37:20 +0200

    trójkąty i kwadraty

---
+++ main.py
@@ -0,0 +1,31 @@
+import string
+import math
+
+
+def pole(dane_figury):
+    if len(dane_figury) == 1:
+        kolo = math.pi*math.pow(float(dane_figury[0]),2)
+        return kolo
+    elif len(dane_figury) == 2:
+        prostokat = float(dane_figury[0])*float(dane_figury[1])
+        return prostokat
+    elif len(dane_figury) == 3:
+        p = (float(dane_figury[0])+float(dane_figury[1])+float(dane_figury[2]))/2
+        trojkat = math.sqrt(p*(p-dane_figury[0])*(dane_figury[1]*dane_figury[2]))
+        return trojkat
+    else:
+        print("Błąd: można podać maksymalnie 3 liczby")
+        quit()
+
+
+liczba_figur = input()
+
+sumka = 0
+
+for i in range(int(liczba_figur)):
+  dane_figury = input()
+  dane_figury = dane_figury.split(' ')
+  sumka += pole(dane_figury)
+
+print(sumka)
+
