df6d275f801be8b3f7337755d81609292a9841b4
Werka <69664@student.ue.poznan.pl>
2022-05-07 20:57:49 +0200

    jakie jest pole tego trójkąta

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import math
 
 def pole(dane_figury):
     if len(dane_figury) == 1:
-        kolo = math.pi*math.pow(float(dane_figury[0]),2)
+        kolo = math.pi*math.pow((dane_figury[0]),2)
         return kolo
     elif len(dane_figury) == 2:
-        prostokat = float(dane_figury[0])*float(dane_figury[1])
+        prostokat = (dane_figury[0])*(dane_figury[1])
         return prostokat
     elif len(dane_figury) == 3:
-        p = (float(dane_figury[0])+float(dane_figury[1])+float(dane_figury[2]))/2
-        trojkat = math.sqrt(p*(p-float(dane_figury[0]))*(p-float(dane_figury[1])*(p-float(dane_figury[2]))))
+        p = ((dane_figury[0])+(dane_figury[1])+(dane_figury[2]))/2
+        trojkat = math.sqrt(p*(p-(dane_figury[0]))*(p-(dane_figury[1])*(p-(dane_figury[2]))))
         return trojkat
     else:
         print("Błąd: można podać maksymalnie 3 liczby")
@@ -25,7 +25,8 @@ sumka = 0
 for i in range(int(liczba_figur)):
   dane_figury = input()
   dane_figury = dane_figury.split(' ')
-  sumka += pole(dane_figury)
+  dane_float = [float(x) for x in dane_figury]
+  sumka += pole(dane_float)
 
 print(round(sumka, 2))
 
